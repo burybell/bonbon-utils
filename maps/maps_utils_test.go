d8889467a18320e4386933679cd082c263d6d087
soup <burybell@163.com>
2021-09-08 01:28:48 +0800

    [feature] 完成带锁map

--- maps/maps_utils_test.go
+++ maps/maps_utils_test.go
@@ -15,6 +15,23 @@ func TestEasyMap(t *testing.T) {
 
 func TestNewMap(t *testing.T) {
 
-	mapped := NewMap().Set("name", "张三").Set("age", 3).Mapped()
+	mapped := NewChainMap().Set("name", "张三").Set("age", 3).Gets()
 	fmt.Println(mapped)
+}
+
+func TestCommonMap(t *testing.T) {
+
+	commonMap := NewCommonMap(20)
+	commonMap.Put("name","张三")
+	commonMap.Put("age",30)
+	fmt.Println(commonMap.Keys())
+}
+
+
+func TestLockCommonMap(t *testing.T) {
+
+	commonMap := NewLockCommonMap(20)
+	commonMap.Put("name","张三")
+	commonMap.Put("age",30)
+	fmt.Println(commonMap.Keys())
 }
